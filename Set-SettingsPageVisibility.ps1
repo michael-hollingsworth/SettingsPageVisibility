34bf11fdc6331a3e1389d9567e5fb043e1c6fcc9
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-09-25 21:44:33 -0400

    Improve setup of Set-SettingsPageVisibility
    Fix comment-based help as well

--- Set-SettingsPageVisibility.ps1
+++ Set-SettingsPageVisibility.ps1
@@ -1,28 +1,32 @@
 <#
 .SYNOPSIS
     Configures the SettingsPageVisibility registry key.
-.PARAMETER Hide
-    Hides the specified settings pages. If settings pages are already hidden, the ones specified will be added to the list of hidden pages. If the SettingsPageVisibility setting is configured in ShowOnly mode, the specified settings pages will be removed from the list of explicitly shown pages.
-.PARAMETER HideOnly
+.PARAMETER HiddenSettings
     Overwrites the SettingsPageVisibility registry property to hide only the specified pages. 
-.PARAMETER Show
-    Removes the specified settings pages from the list of hidden settings pages. Alternatively, if the SettingsPageVisibility setting is configured in ShowOnly mode, the specified settings pages are added to the list of pages that are explicitly shown.
-.PARAMETER ShowOnly
+.PARAMETER ShownSettings
     Specifies which settings pages should be visible. Any pages not included will be hidden.
 .PARAMETER InputObject
     Configures the SettingsPageVisibility registry property based on the configurations inside a `[SettingsPageVisibility]` object.
 .PARAMETER Clear
     Clears the SettingsPageVisibility registry property.
+.PARAMETER Modifier
+    Defines whether to use the Hide or ShowOnly modifier.
+    The Hide modifier only hides settings that are explicitly defined as being hidden.
+    The ShowOnly modifier hides all settings that aren't explicitly defined as being shown.
 .PARAMETER Scope
     Configures the SettingsPageVisibility registry property in the HKEY_CURRENT_USER registry hive instead of the HKEY_LOCAL_MACHINE hive.
 .PARAMETER PassThru
-    Outputs the configured settings page visbility after applying the changes.
+    Passes an object that represents the item to the pipeline. By default, this function does not generate any output.
+.PARAMETER Force
+    Forces the command to run without asking for user confirmation.
 .INPUTS
     InputObject: SettingsPageVisibility
     A SettingsPageVisibility object can be piped into this function. When a SettingsPageVisibility object is piped into this function, the SettingsPageVisibility settings are configured to reflect those set in the object.
 .OUTPUTS
     SettingsPageVisibility
     When the `-PassThru` parameter is used, a SettingsPageVisibility object is returned containing the current configuration.
+.NOTES
+    Author: Michael Hollingsworth
 .LINK
     https://learn.microsoft.com/en-us/windows/configuration/settings/page-visibility
     https://learn.microsoft.com/en-us/windows/iot/iot-enterprise/customize/page-visibility
@@ -59,7 +63,9 @@ function Set-SettingsPageVisibility {
         [Switch]$Clear,
         [Parameter(Mandatory = $false, Position = 1, ParameterSetName = 'HiddenSettings')]
         [Parameter(Mandatory = $false, Position = 1, ParameterSetName = 'ShownSettings')]
-        [SettingsPageVisibilityModifier]$Modifier,
+        [PSDefaultValue(Help = 'Hide')]
+        [SettingsPageVisibilityModifier]$Modifier = [SettingsPageVisibilityModifier]::Hide,
+        [PSDefaultValue(Help = 'Computer')]
         [SettingsPageVisibilityScope]$Scope = [SettingsPageVisibilityScope]::Computer,
         [Switch]$PassThru,
         [Switch]$Force
@@ -67,6 +73,10 @@ function Set-SettingsPageVisibility {
 
     if ($Force -and (-not $PSBoundParameters.ContainsKey('Confirm'))) {
         $ConfirmPreference = [System.Management.Automation.ConfirmImpact]::None
+    }
+
+    if ($Clear) {
+        Clear-SettingsPageVisibility -PassThru:(!!$PassThru) -Force:(!!$Force)
     }
 
     if ($PSCmdlet.ParameterSetName -ne 'InputObject') {
@@ -78,13 +88,7 @@ function Set-SettingsPageVisibility {
             $InputObject.ShownSettings = $ShownSettings
         }
 
-        if ($PSBoundParameters.Containskey('Modifier')) {
-            $InputObject.Modifier = $Modifier
-        }
-
-        if ($Clear) {
-            $InputObject.Clear()
-        }
+        $InputObject.Modifier = $Modifier
     }
 
     if ($PSCmdlet.ShouldProcess($InputObject.Value)) {
