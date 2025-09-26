c224c6ef0e00399766f56d55bebe975df42fca3a
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-09-25 21:43:09 -0400

    Add comment-based help to Get-SettingsPageVisibility

--- Get-SettingsPageVisibility.ps1
+++ Get-SettingsPageVisibility.ps1
@@ -1,8 +1,22 @@
+<#
+.DESCRIPTION
+    Gets the current configuration of the SettingsPageVisibility registry key.
+.PARAMETER Scope
+    Defines whether to retrieve the computer or current user configuration for the SettingsPageVisibility registry key.
+    The default value for this parameter is `Computer`.
+.EXAMPLE
+    Get-SettingsPageVisibility
+.EXAMPLE
+    Get-SettingsPageVisibility -Scope CurrentUser
+.NOTES
+    Author: Michael Hollingsworth
+#>
 function Get-SettingsPageVisibility {
     [CmdletBinding()]
     [OutputType([SettingsPageVisibility])]
     param (
         [Parameter(Position = 0)]
+        [PSDefaultValue(Help = 'Computer')]
         [SettingsPageVisibilityScope]$Scope = [SettingsPageVisibilityScope]::Computer
     )
 
