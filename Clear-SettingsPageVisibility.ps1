9e5760fbd19d36c0a9e94e8996956d4c6949d1b2
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-09-25 21:54:08 -0400

    Add comment-based help to Clear-SettingsPageVisibility

--- Clear-SettingsPageVisibility.ps1
+++ Clear-SettingsPageVisibility.ps1
@@ -1,3 +1,15 @@
+<#
+.DESCRIPTION
+    Clears the SettingsPageVisibility configuration.
+.PARAMETER Scope
+    Configures the SettingsPageVisibility registry property in the HKEY_CURRENT_USER registry hive or the HKEY_LOCAL_MACHINE hive.
+.PARAMETER PassThru
+    Passes an object that represents the item to the pipeline. By default, this function does not generate any output.
+.PARAMETER Force
+    Forces the command to run without asking for user confirmation.
+.EXAMPLE
+    Clear-SettingsPageVisibility -Force
+#>
 function Clear-SettingsPageVisibility {
     [CmdletBinding()]
     param (
