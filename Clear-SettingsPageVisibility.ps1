53d4a84334a7f8b007c048e318c00b18f934d58b
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-09-25 21:25:06 -0400

    Validate that the registry property exists before calling Remove-ItemProperty
    This allows us to set the ErrorAction to Stop so that we can catch any errors that aren't related to the property not existing, such as having incorrect permissions to delete the property

--- Clear-SettingsPageVisibility.ps1
+++ Clear-SettingsPageVisibility.ps1
@@ -1,15 +1,25 @@
 function Clear-SettingsPageVisibility {
     [CmdletBinding()]
     param (
+        [PSDefaultValue(Help = 'Computer')]
         [SettingsPageVisibilityScope]$Scope = [SettingsPageVisibilityScope]::Computer,
+        [Switch]$PassThru,
         [Switch]$Force
     )
 
-    [String]$regKey = if ($Scope -eq [SettingsPageVisibilityScope]::Computer) {
+    [String]$regKeyPath = if ($Scope -eq [SettingsPageVisibilityScope]::Computer) {
         "HKLM:$([SettingsPageVisibility]::spvKeyPath)"
     } else {
         "HKCU:$([SettingsPageVisibility]::spvKeyPath)"
     }
 
-    Remove-ItemProperty -LiteralPath $regKey -Name 'SettingsPageVisibility' -Force:(!!$Force) -ErrorAction Ignore
+    if (-not (($regKey = Get-Item -LiteralPath $regKeyPath -ErrorAction Stop) -and ($regKey.GetValueNames().Contains('SettingsPageVisibility')))) {
+        return
+    }
+
+    Remove-ItemProperty -LiteralPath $regKeyPath -Name 'SettingsPageVisibility' -Force:(!!$Force) -ErrorAction Stop
+
+    if ($PassThru) {
+        return [SettingsPageVisbility]::Get($Scope)
+    }
 }
